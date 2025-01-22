65a23f1905d2eea17fa3b6f053a04a808094e4cd
migli <migli0804.work@gmail.com>
2025-01-22 21:54:23 +0800

    started with portfolio

---
+++ migli_portfolio/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+export default function Home() {
+  return (
+   <main className="relative
+   justfy-center items-center flex-col overflow-hidden max-auto
+   sm:px-10 bg-slate-900">
+      <div className="max-w-7xl w-full">
+       <Hero />
+      </div>
+   </main>
+  );
+}
---
+++ migli_portfolio/tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+/** @type {import('tailwindcss').Config} */
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        },
+      }
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+  ],
+}satisfies Config;
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
