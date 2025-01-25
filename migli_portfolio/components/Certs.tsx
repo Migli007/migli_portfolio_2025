fa2d279e4d14eb46aed6641789564c678077d0be
migli <migli0804.work@gmail.com>
2025-01-26 01:32:04 +0800

    some changes

--- migli_portfolio/components/Certs.tsx
+++ migli_portfolio/components/Certs.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useId, useRef, useState } from "react";
+import React, { useEffect, useId, useRef, useState, } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useOutsideClick } from "@/hooks/use-outside-click";
 
 export function Certs() {
-  const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(null);
+  const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
+    null
+  );
   const id = useId();
-  const ref = useRef<HTMLDivElement | null>(null); // Create a reference with useRef
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     function onKeyDown(event: KeyboardEvent) {
@@ -26,10 +28,10 @@ export function Certs() {
     return () => window.removeEventListener("keydown", onKeyDown);
   }, [active]);
 
-  useOutsideClick(ref, () => setActive(null)); // Pass the whole ref object
+  useOutsideClick(ref, () => setActive(null));
 
   return (
-    <div id="certs">
+    <>
       <AnimatePresence>
         {active && typeof active === "object" && (
           <motion.div
@@ -42,11 +44,31 @@ export function Certs() {
       </AnimatePresence>
       <AnimatePresence>
         {active && typeof active === "object" ? (
-          <div className="fixed inset-0 grid place-items-center z-[100]">
+          <div className="fixed inset-0  grid place-items-center z-[100]">
+            <motion.button
+              key={`button-${active.title}-${id}`}
+              layout
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  duration: 0.05,
+                },
+              }}
+              className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+              onClick={() => setActive(null)}
+            >
+              <CloseIcon />
+            </motion.button>
             <motion.div
               layoutId={`card-${active.title}-${id}`}
-              ref={ref} // This is correct, keep the ref attached to the div
-              className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              ref={ref}
+              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
             >
               <motion.div layoutId={`image-${active.title}-${id}`}>
                 <Image
@@ -55,13 +77,13 @@ export function Certs() {
                   height={200}
                   src={active.src}
                   alt={active.title}
-                  className="w-full h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                  className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
                 />
               </motion.div>
 
               <div>
                 <div className="flex justify-between items-start p-4">
-                  <div>
+                  <div className="">
                     <motion.h3
                       layoutId={`title-${active.title}-${id}`}
                       className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
@@ -75,24 +97,26 @@ export function Certs() {
                       {active.description}
                     </motion.p>
                   </div>
-                </div>
 
-                {/* Chips */}
-                <div className="flex flex-wrap gap-2 px-4 py-2">
-                  {active.chips?.map((chip, index) => (
-                    <span
-                      key={index}
-                      className="px-2 py-1 text-xs rounded-full bg-neutral-200 text-neutral-600 dark:bg-purple-900 dark:text-white"
-                    >
-                      {chip}
-                    </span>
-                  ))}
+                  <motion.a
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    href={active.ctaLink}
+                    target="_blank"
+                    className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
+                  >
+                    {active.ctaText}
+                  </motion.a>
                 </div>
-
                 <div className="pt-4 relative px-4">
                   <motion.div
                     layout
-                    className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
                   >
                     {typeof active.content === "function"
                       ? active.content()
@@ -104,56 +128,43 @@ export function Certs() {
           </div>
         ) : null}
       </AnimatePresence>
-
-      <ul className="pt-5 max-w-9xl mx-auto w-full grid grid-cols-1 md:grid-cols-3 items-start gap-4">
-        {cards.map((card) => (
+      <ul className="max-w-7xl mx-auto w-full grid grid-cols-1 md:grid-cols-3 items-start gap-4">
+        {cards.map((card, index) => (
           <motion.div
             layoutId={`card-${card.title}-${id}`}
             key={card.title}
             onClick={() => setActive(card)}
-            className="p-4 flex flex-col hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+            className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
           >
-            <div className="flex gap-4 flex-col w-full">
+            <div className="flex gap-4 flex-col  w-full">
               <motion.div layoutId={`image-${card.title}-${id}`}>
                 <Image
                   width={100}
                   height={100}
                   src={card.src}
                   alt={card.title}
-                  className="h-60 w-full rounded-lg object-cover object-top"
+                  className="h-60 w-full  rounded-lg object-cover object-top"
                 />
               </motion.div>
               <div className="flex justify-center items-center flex-col">
                 <motion.h3
                   layoutId={`title-${card.title}-${id}`}
-                  className="font-medium text-neutral-800 dark:text-neutral-200 text-center text-base"
+                  className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
                 >
                   {card.title}
                 </motion.h3>
                 <motion.p
                   layoutId={`description-${card.description}-${id}`}
-                  className="text-neutral-600 dark:text-neutral-400 text-center text-base"
+                  className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
                 >
                   {card.description}
                 </motion.p>
-              </div>
-
-              {/* Chips */}
-              <div className="flex flex-wrap gap-2 mt-2">
-                {card.chips?.map((chip, index) => (
-                  <span
-                    key={index}
-                    className="px-2 py-1 text-xs rounded-full bg-neutral-200 text-neutral-600 dark:bg-purple-900 dark:text-white"
-                  >
-                    {chip}
-                  </span>
-                ))}
               </div>
             </div>
           </motion.div>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
 
